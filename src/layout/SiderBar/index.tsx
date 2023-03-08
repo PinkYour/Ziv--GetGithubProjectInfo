7e369b11c69c66ac099e69e4935776cc0ada1db6
许国强 <12461574+Xu-Guo-Qiang@user.noreply.gitee.com>
2023-03-08 15:59:29 +0800

    找到url，完成个人仓库数量的分页

--- src/layout/SiderBar/index.tsx
+++ src/layout/SiderBar/index.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import { Avatar } from 'antd'
+import React ,{useEffect,useState}from 'react'
+import axios from 'axios'
 import './index.scss'
-function index() {
+import Link from 'antd/es/typography/Link'
+
+const Index:React.FC=()=> {
+  const [avatarurl,setAvatarurl]=useState('')
+  const [name,setName]=useState('')
+  const [homepage,setHomepage]=useState('')
+  useEffect(() => {
+    axios.get('https://api.github.com/users/michaelliao').then(
+        response => {
+            // console.log('获取数据成功:side',response.data)
+            setAvatarurl(response.data.avatar_url)
+            setName(response.data.name)
+            setHomepage(response.data.html_url)
+          },
+          error => {
+            console.log('获取数据失败',error)
+          }
+    )
+},[])
   return (
     <div className='SiderBar'>
-        我是siderbar
+
+        <Avatar src={avatarurl} size={100}/> 
+        <br />
+        <ul style={{listStyle:'none',textAlign:'left',lineHeight:3}}>
+          <li><span>姓名: {name}</span></li>
+          <li><Link href={homepage} target="_blank">主页</Link></li>
+        </ul>
     </div>
   )
 }
 
-export default index
+export default Index
